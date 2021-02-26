80060f52bef5d4af1b9bb832991132cb291aef62
jrich8573 <jasonrich85@icloud.com>
2021-02-25 22:28:19 -0500

    troubleshooting alignemt

--- comment.php
+++ comment.php
@@ -8,7 +8,7 @@
         <input type ="radio" name="likes" value="0"> No 
         <br>
         Comments:
-        <textarea  row="4" cols="50" name="comments"></textarea>
+        <textarea  row="4" cols="20" name="comments"></textarea>
         <br>
         <select name='rating' size = 1>
 	        <option value='1'>1</option> 
