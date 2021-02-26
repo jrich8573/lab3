ab8727715aa6473930573ed2548e45c1827fa9cd
jrich8573 <jasonrich85@icloud.com>
2021-02-25 21:10:32 -0500

    corrected insert statement to account for timestamp element

--- process_comment.php
+++ process_comment.php
@@ -3,14 +3,50 @@
     $userName=$_POST["username"];
     echo "Hello <b>$userName<b>!<br>";
 
-    // fetch rows in the table
-while( $row = $result->fetch_assoc() ) {
-    echo "<tr>\n";
-    foreach ($row as $cell){
-        echo "<td> $cell </td>";
-    }
-     echo "</tr>\n"; 
-}
 
+    // connection string
+    $server = 'imysql-class.infra.cs.odu.edu';
+	$sqlUsername = 'jrich202020';
+	$sqlPassword = $_ENV["MYPW"];
+	$databaseName = 'jrich202020db';
+
+	$con = new mysqli($server, $sqlUsername, $sqlPassword);
+
+	//check connection
+	if($conn-> connection_error){
+			die("Connection failed: " .$conn->connect_error);
+	}
+      echo "Connection passed!";
+
+    //insert data into user table
+    $sql = "INSERT INTO $table VALUES (NULL'$username','$likes', '$comments', '$rating')";
+
+    //execute sql
+    $query_result = $conn->query($sq)
+        or die ("SQL Error: Data Insert is No Bueno!");
+
+    //call the user table 
+    $sql = "SELECT * FROM $table";
+    echo "Query: ".$sql."<br>";
+    $result = $conn->query($sql)
+        or die("ERROR: Query is No Bueno!");
+    
+    echo "<tr>";
+    // fetch attribute names
+     while ($fieldMetadata = $result->fetch_field()){
+        echo "<th>".$fieldMetadata->name."</th>"; 
+    }
+     echo "</tr>";
+
+    // fetch rows in the table
+    while($row = $result->fetch_assoc()){
+        echo "<tr>\n";
+        foreach ($row as $cell){
+            echo "<td> $cell </td>";
+        }
+         echo "</tr>\n"; 
+    }
+    $result->free();
+    $conn->close();
 
 ?>
