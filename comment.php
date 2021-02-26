ecf6c9e05a51d399cec8c54945e7d78775a5a749
jrich8573 <jasonrich85@icloud.com>
2021-02-25 20:42:12 -0500

    added buttons to html and sql to php files

--- comment.php
+++ comment.php
@@ -3,6 +3,13 @@
         Name:
         <input type = "text" name ="username" vaule =""/>
         <br>
+        <select name= 'StudentID' size = 1>
+	        <option value='1'>1</option> 
+	        <option value='2'>2</option>
+        	<option value='3'>3</option> 
+            <option value='4'>4</option> 
+        </select>
+        <br>
         <input type = "submit" value = "go"/>
     </form>
 </html>
--- process_comment.php
+++ process_comment.php
@@ -3,4 +3,14 @@
     $userName=$_POST["username"];
     echo "Hello <b>$userName<b>!<br>";
 
+    // fetch rows in the table
+while( $row = $result->fetch_assoc() ) {
+    echo "<tr>\n";
+    foreach ($row as $cell){
+        echo "<td> $cell </td>";
+    }
+     echo "</tr>\n"; 
+}
+
+
 ?>
