385d6163fc39fb2a5ee5f73120408f21bb47c7e9
jrich8573 <jasonrich85@icloud.com>
2021-02-25 20:14:19 -0500

    initial commit with code

---
+++ comment.php
@@ -0,0 +1,9 @@
+<html>
+    <form action="process_comment.php" method="post">
+        Name:
+        <input type = "text" name ="username" vaule =""/>
+        <br>
+        <input type = "submit" value = "go"/>
+    </form>
+</html>
+
---
+++ dbtest.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$server = 'imysql-class.infra.cs.odu.edu'
+	$sqlUsername = 'jrich202020'
+	$sqlPassword = ''
+	$databaseName = 'jrich202020db'
+
+	$con = new mysqli($server, $sqlUsername, $sqlPassword);
+
+	//check connection
+	if($conn-> connection_error){
+			die("Connection failed: " .$conn->connect_error);
+	}
+  echo "Connection passed!";
+?>
