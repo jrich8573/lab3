b5e2444ef6943b2a48a70efbddbedf8c93c6d40b
jrich8573 <jasonrich85@icloud.com>
2021-02-25 21:00:06 -0500

    added radio buttons to html

--- comment.php
+++ comment.php
@@ -3,11 +3,16 @@
         Name:
         <input type = "text" name ="username" vaule =""/>
         <br>
+        Do you like this page? 
+        <input type ="radio" name="like" value="1"> Yes 
+        <input type ="radio" name="like" value="0"> No 
+        <br>
         <select name= 'StudentID' size = 1>
 	        <option value='1'>1</option> 
 	        <option value='2'>2</option>
         	<option value='3'>3</option> 
             <option value='4'>4</option> 
+            <option value='5'>5</option>
         </select>
         <br>
         <input type = "submit" value = "go"/>
